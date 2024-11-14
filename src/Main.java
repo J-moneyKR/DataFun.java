8d21eeeeee41c3c403016408f2389427eba00b2c
fryerj_wlhs <fryerj@wlhs.wlwv.k12.or.us>
2024-11-14 14:57:09 -0800

    tyeshit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -26,7 +28,7 @@ public class Main {
             System.out.println("This number odd tye shi");
         }
         // Most of this is not my own work and I had to use a torutiral to find out what a swithc is
-        switch (favint){
+        switch (favint) {
             case 10:
                 System.out.println("This number is a neon");
                 break;
@@ -48,7 +50,40 @@ public class Main {
             default:
                 System.out.println("This number is NOT a noble gas tye shi");
         }
+                Scanner scanaylized = new Scanner(System.in);
+                System.out.println("You got to give me your favroite character");
+            char charactrer = scanaylized.next().charAt(0);
+            if (Character.isUpperCase(charactrer)) { // from interner is uppercase
+                System.out.println("This number is a uppercase character");
+            }
+            else if (Character.isLowerCase(charactrer)) { //from internet is lowercase
+                System.out.println("This is a lowercase number");
+            }
+            //is digit was looked up on the interent
+            else if (Character.isDigit(charactrer)) {
+                System.out.println("This is a number ");
+            }
+            else{
+                System.out.println("This is not a letter or a number from the english language");
+                }
+            // I used the internet to look up how to make a list in java
+        List<Character> vowellist = Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'y', 'Y');
+            //looked up the .containes thing from the internet
+            if (vowellist.contains(charactrer)){
+                System.out.println("This is a vowel");
+            } else {
+                System.out.println("This is not a vowel");
+            }
+            //This part was looked up form the internet by casting it
+            int valueofChar = (int) charactrer;
+            System.out.println("The ascii and or unicode value which is from the int of all of coding is " + valueofChar);
+            if ((valueofChar >= 65 && valueofChar <= 90) || (valueofChar >= 97 && valueofChar <= 122)){
+                int value = (int) charactrer;
+                // switch statment with logic for each one inside
+            } else {
+                System.out.println("This is not a letter in the english alphabet");
 
+            }
 
     }
-}
+    }
